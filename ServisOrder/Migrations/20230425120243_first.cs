470c3b1baeede65ed42d9a987eb55c3ec1d3b684
JustKaneri <justkaneri@yandex.ru>
2023-04-25 15:03:19 +0300

    Create db for order servis

---
+++ ServisOrder/Migrations/20230425120243_first.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ServisOrder.Migrations
+{
+    /// <inheritdoc />
+    public partial class first : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    ProductId = table.Column<int>(type: "integer", nullable: false),
+                    CreateOrder = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Order");
+        }
+    }
+}
---
+++ ServisOrder/Model/Order.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ServisOrder.Model
+{
+    [Table("Order")]
+    public class Order
+    {
+        [Key]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Column("UserId")]
+        [Required]
+        public int UserId { get; set; }
+
+        [Column("ProductId")]
+        [Required]
+        public int ProductId { get; set; }
+
+        [Column("CreateOrder")]
+        public DateTime? CreateDate { get; set; } = DateTime.Now;
+    }
+}
